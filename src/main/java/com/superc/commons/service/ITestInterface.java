5e588b62c5f82c959a5b8a2ae440445f22d7f0a2
super <858994555@qq.com>
2015-08-28 17:58:26 +0800

    初始化

---
+++ src/main/java/com/superc/commons/service/ITestInterface.java
@@ -0,0 +1,7 @@
+package com.superc.commons.service;
+
+/**
+ * Created by super on 2015/8/28.
+ */
+public interface ITestInterface {
+}
---
+++ src/main/java/com/superc/commons/service/impl/TestInterfaceImpl.java
@@ -0,0 +1,11 @@
+package com.superc.commons.service.impl;
+
+import com.superc.commons.service.ITestInterface;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by super on 2015/8/28.
+ */
+@Service("testService")
+public class TestInterfaceImpl  implements ITestInterface{
+}
